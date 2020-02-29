ef87502db1971fb2422bb53e13880def88b7bef3
Andrew-Miner <andrew.miner3@gmail.com>
2020-02-29 14:17:29 -0800

    Added Lab 1

---
+++ Lab 0/README.md
@@ -0,0 +1,13 @@
+
+Author: Andrew Miner
+
+Class: Comp 322-16820
+
+
+2/29/20 - 12:30pm 
+	Created Lab 1 folder and started working on the project
+
+2/29/20 - 2:15pm
+	Finished project and uploaded it to Github
+
+Project Status: Finished and Working!
---
+++ Lab 1/time-4-baby-and-me.c
@@ -0,0 +1,43 @@
+// Author: Andrew Miner
+// Date: 2/29/2020
+// Class: Comp 322
+#include <sys/times.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void printPIDs(int child, int retVal)
+{
+    printf("PPID: %d, PID: %d", getppid(), getpid());
+    
+    if(child != 0)
+        printf(", CPID: %d, RETVAL: %d", child, retVal);
+    
+    printf("\n");
+}
+
+int main(int argc, char* argv[])
+{
+    printf("Start: %ld\n", time(NULL));
+    
+    struct tms buf;
+    
+    int retVal = -1;
+    int child = fork();
+    
+    waitpid(child, &retVal, 0);
+    printPIDs(child, retVal);
+    
+    if(child == 0)
+        exit(0);
+    
+    times(&buf);
+    printf("USER: %ld, SYS: %ld\n", buf.tms_utime, buf.tms_stime);
+    printf("CUSER: %ld, CSYS: %ld\n", buf.tms_utime, buf.tms_stime);
+    printf("Stop: %ld\n", time(NULL));
+    
+	return 0;
+}
